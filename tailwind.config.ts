2927813af159f24b5a56b9c6f87d6fde418a65b0
manuelwebdev <mfewcar@gmail.com>
2024-01-14 22:38:02 -0700

    Setting up prettier, shadcn ui, Building out first section of layout on mobile.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      clipPath: {
+        documentShape: 'polygon(23% 0, 100% 0, 100% 99%, 0 100%, 0 21%)',
+      },
     },
   },
   plugins: [],
