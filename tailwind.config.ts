78c8cd128779285d6ec4fd2e4490f0e02519cab4
manuelwebdev <mfewcar@gmail.com>
2024-01-15 20:52:11 -0700

    Got middleware working at /welcome to display data from storage.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        dosis: ['Dosis', 'sans-serif'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
