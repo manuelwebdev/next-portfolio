2b04f7637f1520a9ac3591666b5bfa10662cb656
manuelwebdev <mfewcar@gmail.com>
2024-01-27 16:42:18 -0700

    resume file. styled icon buttons.

--- app/socials/SocialsList.tsx
+++ app/socials/SocialsList.tsx
@@ -2,7 +2,12 @@ import { Button, buttonVariants } from '@/components/ui/button'
 import { get } from '@vercel/edge-config'
 import Link from 'next/link'
 import { IconContext } from 'react-icons'
-import { FaLinkedinIn } from 'react-icons/fa6'
+import {
+  FaLinkedinIn,
+  FaGithub,
+  FaFileContract,
+  FaEnvelope,
+} from 'react-icons/fa6'
 
 // get socials from server
 type Social = {
@@ -18,18 +23,39 @@ export default async function SocialsList() {
   const socials = await getSocials()
   return (
     <>
-      {socials?.map((social: Social) => (
-        <>
-          <Link
-            key={social?.name}
-            href={social?.link}
-            className={`p-3 border-2 border-solid border-black flex flex-col items-center justify-center rounded-md`}
-          >
-            <FaLinkedinIn />
-            {/* {social?.name} */}
-          </Link>
-        </>
-      ))}
+      {socials?.map((social: Social) => {
+        console.log(social.name)
+        let socialIcon
+        switch (social?.name) {
+          case 'LinkedIn':
+            socialIcon = <FaLinkedinIn />
+            break
+          case 'Github':
+            socialIcon = <FaGithub />
+            break
+          case 'Resume':
+            socialIcon = <FaFileContract />
+            break
+          case 'Email':
+            socialIcon = <FaEnvelope />
+            break
+
+          default:
+            break
+        }
+        return (
+          <>
+            <Link
+              key={social?.name}
+              href={social?.link}
+              className={`p-3 border-2 border-solid border-black flex flex-col items-center justify-center rounded-full`}
+            >
+              {socialIcon}
+              {/* {social?.name} */}
+            </Link>
+          </>
+        )
+      })}
     </>
   )
 }
