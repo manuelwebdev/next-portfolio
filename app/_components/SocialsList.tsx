dbcb0758b80307ce15a928356651de5d32079655
manuelwebdev <mfewcar@gmail.com>
2024-02-27 21:33:05 -0700

    Added key to social list element.

--- app/_components/SocialsList.tsx
+++ app/_components/SocialsList.tsx
@@ -38,7 +38,7 @@ export default function SocialsList({ socials }: { socials: Social[] | any }) {
             break
         }
         return (
-          <Tooltip.Provider>
+          <Tooltip.Provider key={social?.name}>
             <Tooltip.Root delayDuration={200}>
               <Tooltip.Trigger asChild>
                 <Link
@@ -64,7 +64,7 @@ export default function SocialsList({ socials }: { socials: Social[] | any }) {
               </Tooltip.Trigger>
               <Tooltip.Portal>
                 <Tooltip.Content
-                  className="data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade text-violet11 select-none rounded-[4px] bg-white px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity]"
+                  className="text-violet11 select-none rounded-[4px] bg-white px-[15px] py-[10px] text-[15px] leading-none shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] will-change-[transform,opacity] data-[state=delayed-open]:data-[side=bottom]:animate-slideUpAndFade data-[state=delayed-open]:data-[side=left]:animate-slideRightAndFade data-[state=delayed-open]:data-[side=right]:animate-slideLeftAndFade data-[state=delayed-open]:data-[side=top]:animate-slideDownAndFade"
                   sideOffset={5}
                 >
                   {social?.name}
